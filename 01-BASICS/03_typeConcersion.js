b53046a7cff3c099b52a0bd45489fbfb076efa90
ItsProgrammerVinay <119567736+ItsProgrammerVinay@users.noreply.github.com>
2023-09-30 07:26:38 +0000

    type conversion

---
+++ 01-BASICS/03_typeConcersion.js
@@ -0,0 +1,17 @@
+let nums="33"
+console.log(Number(nums));
+console.log(typeof nums);
+
+let isLogedIn="1" //convertion 1 into true and 0 into false
+let toBoolean =Boolean(isLogedIn)
+
+
+
+console.log(Boolean (toBoolean));
+console.log(typeof toBoolean);
+
+
+let element ="3asd"
+let toNumber=Number (element);
+console.log(toNumber);// it returns null
+console.log( typeof toNumber);
