11079db091c73b8a62b3a2b92982fad6d778e467
ItsProgrammerVinay <119567736+ItsProgrammerVinay@users.noreply.github.com>
2023-09-30 14:22:39 +0000

    comparision operator

---
+++ 01-BASICS/04_comparion.js
@@ -0,0 +1,16 @@
+console.log(2>1);
+console.log(2<=1);
+console.log(2==1);
+console.log(2>=6);
+console.log(21>1);
+console.log(21!=1);
+
+//
+console.log(null>0);
+console.log(null==0);
+console.log(null<=0);//gives true
+console.log(null>=0);//true
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<=0);
