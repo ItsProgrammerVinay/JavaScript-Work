e4c86d159e2745da2d7faef039969250fe11ad14
ItsProgrammerVinay <119567736+ItsProgrammerVinay@users.noreply.github.com>
2023-09-30 07:28:02 +0000

    datatypes

--- 01-BASICS/01_variables.js
+++ 01-BASICS/01_variables.js
@@ -3,4 +3,4 @@ let accountEmail="vinaygole880@gmail.com"
 var accountPass="11335"
 accountCity="Mumbai"
 
-console.table([accountId,accountEmail,accountPass,accountId])
+console.table([accountId,accountEmail,accountPass,accountCity])
---
+++ 01-BASICS/02_datatypes.js
@@ -0,0 +1,12 @@
+let num=123
+let name="Shrinidhi"
+let weight=72.33
+let isRight=true
+let address=null
+let roll;
+
+console.log(typeof num)
+console.log(typeof isRight)
+console.log(typeof weight);//float value have also number type
+console.log(typeof address);//null have oject type
+console.log(typeof roll); //undefined have undefined type
