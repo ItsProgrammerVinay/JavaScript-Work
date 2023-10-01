0862a89eda581d923f10a8006486f76cbef61879
ItsProgrammerVinay <119567736+ItsProgrammerVinay@users.noreply.github.com>
2023-10-01 09:02:59 +0000

    arrays  and its methods

---
+++ 02-BASICS/01_Arrays.js
@@ -0,0 +1,30 @@
+//Arrays
+
+const arr=[0,1,2,3,4,56,6]
+// console.log(arr[2]);//index start from 0
+
+// const actors=["Pankaj tripathi","Manoj vajpai","Irfan khan","Tony stark"]
+// arr.push(5)
+// arr.pop(56)
+// arr.unshift(9)
+// arr.shift()
+// console.log(arr);
+// console.log(actors);
+
+// console.log(arr.includes(3));
+
+// console.log(arr.indexOf(2));
+
+// const newArr=arr.join()
+// console.log(arr);
+// console.log(newArr);//type is string
+
+
+console.log("A",arr);
+ const myn1=arr.slice(1,3)
+ console.log(myn1);
+
+ console.log("B",arr);
+ const mym2=arr.splice(1,3)
+ console.log(mym2);
+ 
