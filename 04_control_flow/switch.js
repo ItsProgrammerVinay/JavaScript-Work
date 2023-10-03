cfc7345783cf96901f787966781a7a322939a030
ItsProgrammerVinay <119567736+ItsProgrammerVinay@users.noreply.github.com>
2023-10-03 15:51:40 +0000

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,36 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month="mach"
+
+switch (month) {
+    case "jan":
+
+    console.log("January");
+        break;
+
+    case  "feb":
+
+    console.log("feb");
+    break;
+
+    case  "march":
+
+    console.log("March");
+    break;
+
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+
+    console.log("default case match: ");
+        break;
+}
